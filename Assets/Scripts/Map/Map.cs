888fbbdba54874dc407932fbdfd0ad236317d464
khoa <averageazn1337@gmail.com>
2015-02-26 10:04:21 -0600

    added clickable char.

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+	public bool isPlayerSelected = false;
+	public bool isPointSelected = false;
+	public PointControl point;
+	private Vector3 mousePos;
+	public PlayerControl player;
+	public Transform playerStart, playerEnd;
+	// Use this for initialization
+	void Start () {
+	
+		ArrayList points;
+		ArrayList vistedpoints;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Raycasting (); 
+		MouseSelection ();
+	}
+	void Raycasting(){
+		//Debug.DrawLine (playerStart.position, playerEnd.position, Color.red);
+		isPointSelected = Physics2D.Linecast (Input.mousePosition, Input.mousePosition, 1 << LayerMask.NameToLayer ("point"));
+		Debug.Log (isPointSelected+ " ray result");
+		if (isPointSelected) {
+			Debug.Log("Moving to point");
+		}
+
+	}
+
+	void MouseSelection(){
+		if (Input.GetMouseButton (0)) {
+			//Debug.Log (Input.mousePosition);
+			Debug.Log ("gaME obj" + gameObject.tag +" "+gameObject.name);
+		} else if (Input.GetMouseButton (1) && isPlayerSelected) {
+			Raycasting (); 
+			//Debug.Log ("do ray"+isPlayerSelected);
+		}
+	}
+}
---
+++ Assets/Scripts/Map/PointControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointControl : MonoBehaviour {
+	public int x,y;
+	public bool visted = false;
+	public EventControl pointEvent;
+	public bool isSelected = false;
+	public PlayerControl player;
+	public Map map;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isSelected) {
+			gameObject.renderer.material.color = Color.blue;
+			 
+		} else {
+			this.gameObject.renderer.material.color = Color.white;
+		}
+
+	}
+
+	void OnMouseOver(){
+		//this.isSelected = true;
+		 
+		if (Input.GetMouseButtonDown (1)) {
+			Debug.Log (" point is selected ");
+		}
+
+
+
+		
+	}
+}
