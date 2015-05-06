ea4e5e00881712bbbfbbf66a18d0e98f5dacd7b3
khoa <averageazn1337@gmail.com>
2015-05-06 14:53:51 -0500

    game starts unpaused if no tutorial selected

--- Assets/Scripts/UI/GameUI/Tutorial.cs
+++ Assets/Scripts/UI/GameUI/Tutorial.cs
@@ -219,6 +219,10 @@ public class Tutorial : MonoBehaviour {
 		}
 		if (GUI.Button(new Rect(50, 50, 50, 20), "No")) {
 			currentState = TutorialStates.State.Off;
+			if(gC.IsPaused())
+			{
+				gC.TogglePauseGame();
+			}
 		}
 	}
 }
